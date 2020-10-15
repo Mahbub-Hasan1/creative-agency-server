ce0dcc2daac83bfc74aff8f9fcaf098370a37be7
Md-Mahbub-Ahmed <67556397+Md-Mahbub-Ahmed@users.noreply.github.com>
2020-10-15 22:44:04 +0600

    done

--- index.js
+++ index.js
@@ -24,12 +24,13 @@ app.get('/', (req, res) => {
 })
 
 
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("Agency").collection("AgencyServices");
   const OrderCollection = client.db("Agency").collection("AllOrder");
   const reviewCollection = client.db("Agency").collection("Review");
+  const adminCollection = client.db("Agency").collection("Addmin");
+
 
   app.post('/addService', (req, res) => {
     const file = req.files.file;
@@ -40,73 +41,106 @@ client.connect(err => {
     const encImg = newImg.toString('base64');
 
     var image = {
-        contentType: file.mimetype,
-        size: file.size,
-        img: Buffer.from(encImg, 'base64')
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, 'base64')
     };
 
     productsCollection.insertOne({ name, email, image, Description })
-        .then(result => {
-            res.send(result.insertedCount > 0);
-        })
-})
+      .then(result => {
+        res.send(result.insertedCount > 0);
+      })
+  })
 
-    app.get('/service', (req, res) => {
-      productsCollection.find({})
-      .toArray( (err, documents) => {
+  app.get('/service', (req, res) => {
+    productsCollection.find({})
+      .toArray((err, documents) => {
         res.send(documents);
       })
-    })
+  })
 
-    app.get('/service/item/:_id', (req, res) => {
-      productsCollection.find({_id: ObjectId(req.params._id)})
-      .toArray( (err, documents) => {
+  app.get('/service/item/:_id', (req, res) => {
+    productsCollection.find({ _id: ObjectId(req.params._id) })
+      .toArray((err, documents) => {
         res.send(documents[0]);
       })
-    })
+  })
 
 
-    app.post('/addOrder', (req, res) => {
-      const file = req.files.file;
-      const name = req.body.name;
-      const email = req.body.email;
-      const newImg = file.data;
-      const encImg = newImg.toString('base64');
+  app.post('/addOrder', (req, res) => {
+    const file = req.files.file;
+    const name = req.body.name;
+    const serviceName = req.body.serviceName;
+    const email = req.body.email;
+    const description = req.body.description;
+    const newImg = file.data;
+    const encImg = newImg.toString('base64');
 
-      var image = {
-          contentType: file.mimetype,
-          size: file.size,
-          img: Buffer.from(encImg, 'base64')
-      };
+    var image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, 'base64')
+    };
 
-      OrderCollection.insertOne({ name, email, image })
-          .then(result => {
-              res.send(result.insertedCount > 0);
-          })
+    OrderCollection.insertOne({ name, serviceName, email, description, image })
+      .then(result => {
+        res.send(result.insertedCount > 0);
+      })
   })
 
   app.get('/Orders', (req, res) => {
-    OrderCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
-        })
-});
-
-app.post('/addReview', (req, res) => {
-  const reviewInfo = req.body;
-  reviewCollection.insertOne(reviewInfo)
-  .then(result => {
-    res.send(result.insertedCount > 0)
-  })
-})
-
-app.get('/review', (req, res) => {
-  reviewCollection.find({})
+    OrderCollection.find({ email: req.query.email })
       .toArray((err, documents) => {
-          res.send(documents);
+        res.send(documents);
       })
-});
+  });
 
+  // app.patch('/update/:id', (req, res) => {
+  //   OrderCollection.updateOne({_id: ObjectId(req.query.id)},
+  //   {
+  //     $set: {}
+  //   })
+  //   .then (result => {
+  //     console.log(result)
+  //   })
+  // })
+
+  app.get('/allServiceList', (req, res) => {
+    OrderCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  });
+
+  app.post('/addReview', (req, res) => {
+    const reviewInfo = req.body;
+    reviewCollection.insertOne(reviewInfo)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.get('/review', (req, res) => {
+    reviewCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  });
+
+  app.post('/addAdmin', (req, res) => {
+    const adminInfo = req.body;
+    adminCollection.insertOne(adminInfo)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.get('/admin', (req, res) => {
+    adminCollection.find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  });
 
 });
 
